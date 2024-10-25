fcadebc52f65eab07be698ae1a550ea678c37fdf
sailakshmi012000 <165908224+sailakshmi012000@users.noreply.github.com>
2024-10-25 17:25:50 +0530

    Create ai-leads-tomcat.groovy

---
+++ vars/ai-leads-tomcat.groovy
@@ -0,0 +1,11 @@
+def call(credId,user,ip){
+      sshagent(['credId']){
+                    //rename
+                    sh "mv target/ai-leads*.war target/leads.war"
+                    //copy war
+                    sh "scp -o StrictHostKeyChecking=no target/leads.war user@ip:/opt/tomcat9/webapps"
+                    //stop&start jenkins
+                    sh "ssh user@ip /opt/tomcat9/bin/shutdown.sh"
+                    sh "ssh user@ip /opt/tomcat9/bin/startup.sh"
+                }
+}
