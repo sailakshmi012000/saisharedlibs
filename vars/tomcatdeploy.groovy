ec81d6f13b9e9166348fd626d0600e6abca275fc
sailakshmi012000 <165908224+sailakshmi012000@users.noreply.github.com>
2024-10-25 19:31:29 +0530

    Create tomcatdeploy.groovy

---
+++ vars/tomcatdeploy.groovy
@@ -0,0 +1,12 @@
+def call(credId,user,ip){
+   sshagent([credId]) {
+                // rename
+                sh 'mv target/myweb-0.0.9*.war target/myweb.war'
+                //copy
+                sh 'scp -o StrictHostKeyChecking=no target/myweb.war ${user}@${ip}:/opt/tomcat9/webapps'
+                //stop
+                sh 'ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh'
+                //start
+                sh 'ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh'
+                }
+}
