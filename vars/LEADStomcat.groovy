67d909977250bf69acc13732c74ce7b868622cd1
sailakshmi012000 <165908224+sailakshmi012000@users.noreply.github.com>
2025-03-26 14:25:25 +0530

    Create LEADStomcat.groovy

---
+++ vars/LEADStomcat.groovy
@@ -0,0 +1,10 @@
+def call(ip,user,cred){
+                sshagent([cred]) {
+                    // copy war file
+                    sh "scp -o StrictHostKeyChecking=no target/ai-leads.war ${user}@${ip}:/opt/tomcat9/webapps"
+                    // shutdown
+                    sh "ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
+                    //start
+                    sh "ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
+                }
+}
