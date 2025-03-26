fd7dafcb6507a5e2ac14a72ef9c68d9d167aaf48
sailakshmi012000 <165908224+sailakshmi012000@users.noreply.github.com>
2025-03-26 14:11:59 +0530

    Update ai-leads-tomcat.groovy

--- vars/ai-leads-tomcat.groovy
+++ vars/ai-leads-tomcat.groovy
@@ -1,4 +1,4 @@
-def call (ip,user,cred){
+def call(ip,user,cred){
                 sshagent([cred]) {
                     // copy war file
                     sh "scp -o StrictHostKeyChecking=no target/ai-leads.war ${user}@${ip}:/opt/tomcat9/webapps"
